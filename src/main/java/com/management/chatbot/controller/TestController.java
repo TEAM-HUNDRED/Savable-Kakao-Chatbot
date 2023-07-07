3d50a6fbdb0c8c7442c8ae7643743e05dfae8d76
DAY0522 <ekdud_____@naver.com>
2023-07-08 01:45:45 +0900

    feat: "카카오톡 메시지 json 반환 API 구현"
    카카오톡 메시지 스킬로 연결했을 때 보낸 메시지에 대한 json data를 반환하는 기능

---
+++ src/main/java/com/management/chatbot/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.management.chatbot.controller;
+
+import com.management.chatbot.service.dto.KakaoResponseDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class TestController {
+
+    @PostMapping("/test") // json 형식을 그대로 출력하는 코드
+    public HashMap<String, Object> returnJson(@RequestBody String json) {
+        return new KakaoResponseDto().makeResponseBody(json);
+    }
+}
