a8661562642823d5ef3053fd2e0ed4ba26b1fae5
DAY0522 <ekdud_____@naver.com>
2023-07-13 01:36:25 +0900

    test: "서버 시간 확인을 위한 테스트 코드 추가"

--- src/main/java/com/management/chatbot/controller/TestController.java
+++ src/main/java/com/management/chatbot/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.management.chatbot.controller;
 
 import com.management.chatbot.service.dto.KakaoResponseDto;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 
 @RestController
@@ -12,6 +16,6 @@ public class TestController {
 
     @PostMapping("/test") // json 형식을 그대로 출력하는 코드
     public HashMap<String, Object> returnJson(@RequestBody String json) {
-        return new KakaoResponseDto().makeResponseBody(json);
+        return new KakaoResponseDto().makeResponseBody(json + LocalDate.now().toString());
     }
 }
