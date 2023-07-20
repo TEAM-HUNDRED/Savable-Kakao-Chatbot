7eb278d58ad1213945bcd8c398940528e994953e
DAY0522 <ekdud_____@naver.com>
2023-07-21 02:51:26 +0900

    feat: "ChatbotApplication.java 파일 추가"

---
+++ src/main/java/com/management/ChatbotApplication.java
@@ -0,0 +1,20 @@
+package com.management;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class ChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotApplication.class, args);
+	}
+
+	@PostConstruct
+	void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+}
