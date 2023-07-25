de8de765a620ff36fd7da65132a845aa3b32a1b4
DAY0522 <ekdud_____@naver.com>
2023-07-26 02:46:09 +0900

    fix: "랭킹 버튼 클릭 시 메시지 잘리는 현상 수정"
    - 랭킹 버튼 클릭 시 메시지가 잘리는 현상을 수정하였습니다.

--- src/main/java/com/management/chatbot/controller/MemberController.java
+++ src/main/java/com/management/chatbot/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.management.chatbot.service.MemberService;
 import com.management.chatbot.service.dto.KakaoBasicCardResponseDto;
 import com.management.chatbot.service.dto.KakaoDto.BasicCard;
 import com.management.chatbot.service.dto.KakaoDto.ButtonDto;
+import com.management.chatbot.service.dto.KakaoDto.SimpleTextDto;
 import com.management.chatbot.service.dto.KakaoRequestDto;
 import com.management.chatbot.service.dto.KakaoResponseDto;
 import com.management.chatbot.service.dto.MemberSaveRequestDto;
@@ -55,16 +56,9 @@ public class MemberController {
                 .build();
         buttonDtoList.add(buttonDto);
 
+        // 메시지 1
         String message = "세이버님의 현재 랭킹이 궁금하신가요\uD83D\uDE06?\n" +
-                "아래 '랭킹 보러가기' 버튼을 눌러 세이버님의 랭킹을 확인하세요!\n" +
-                "\n" +
-                "성실히 참여한 세이버분들께 아래와 같은 보상을 지급하고 있습니다❤\uFE0F\u200D\uD83D\uDD25\n" +
-                "\uD83D\uDCCC 랭킹 보상 금액\n" +
-                "▪\uFE0F 1등: 1,000원\n" +
-                "▪\uFE0F 2등: 800원\n" +
-                "▪\uFE0F 3등: 500원\n" +
-                "▪\uFE0F 4, 5등: 300원\n" +
-                "(랭킹은 매주 일요일 11:59PM에 초기화되며, 월요일에 보상이 지급됩니다.)";
+                "하단의 버튼을 눌러 세이버님의 랭킹을 확인하세요!";
         BasicCard basicCardDto = BasicCard.builder()
                 .title(message)
                 .thumbnail(BasicCard.Thumbnail.builder()
@@ -73,10 +67,27 @@ public class MemberController {
                 .buttons(buttonDtoList)
                 .build();
 
+        // 메시지 2
+        String message2 = "성실히 참여한 세이버분들을 위한 보상이 있어야겠죠?❤\uFE0F\u200D\uD83D\uDD25\n순위에 따라 아래와 같이 보상을 드립니다!" +
+                "\uD83D\uDCCC 랭킹 보상 금액\n" +
+                "▪\uFE0F 1등: 1,000원\n" +
+                "▪\uFE0F 2등: 800원\n" +
+                "▪\uFE0F 3등: 500원\n" +
+                "▪\uFE0F 4, 5등: 300원\n" +
+                "(랭킹은 매주 일요일 11:59PM에 초기화되며, 월요일에 보상이 지급됩니다.)";
+
+        SimpleTextDto simpleTextDto = SimpleTextDto.builder()
+                .text(message2)
+                .build();
+
+        HashMap<String, Object> simpleText = new HashMap<>();
+        simpleText.put("simpleText", simpleTextDto);
+
         List<HashMap<String, Object>> outputs = new ArrayList<>();
         HashMap<String, Object> basicCard = new HashMap<>();
         basicCard.put("basicCard", basicCardDto);
         outputs.add(basicCard);
+        outputs.add(simpleText);
         return new KakaoBasicCardResponseDto().makeResponseBody(outputs);
     }
 
