12b36305b34597147a6456f4ce2236c110c7dbc0
DAY0522 <ekdud_____@naver.com>
2023-07-27 22:09:08 +0900

    feat: "webConfig 파일 추가"
    - cors 에러 해결을 위한 webConfig 파일을 추가

---
+++ src/main/java/com/management/web/config/webConfig.java
@@ -0,0 +1,17 @@
+package com.management.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://savable.net",
+                        "http://172.16.101.189:3000/",
+                        "http://savable-content.s3-website.ap-northeast-2.amazonaws.com/")
+                .allowedMethods("*");
+}
