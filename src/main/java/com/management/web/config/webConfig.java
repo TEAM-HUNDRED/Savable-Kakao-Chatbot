2b155a09c631ceb7fec6cfce224bca7578654333
DAY0522 <ekdud_____@naver.com>
2023-07-27 22:11:14 +0900

    style: "세미콜론 누락 수정"

--- src/main/java/com/management/web/config/webConfig.java
+++ src/main/java/com/management/web/config/webConfig.java
@@ -11,7 +11,9 @@ public class webConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://savable.net",
+                        "https://savable.net",
                         "http://172.16.101.189:3000/",
                         "http://savable-content.s3-website.ap-northeast-2.amazonaws.com/")
                 .allowedMethods("*");
+    }
 }
